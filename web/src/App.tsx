f0ad174450a107594131860766f18a7785e7b78f
rohits-web03 <rohit.dey302003@gmail.com>
2025-10-11 16:46:21 +0530

    refactor share routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,22 @@
-import Login from "./components/Login"
-import Register from "./components/Register"
-import FileUpload from "./components/FileUpload";
-import FileReceive from "./components/FileReceive";
+import Login from "@/components/Login"
+import Register from "@/components/Register"
+import FileUpload from "@/components/FileUpload";
+import FileReceive from "@/components/FileReceive";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-
-
-import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/register"/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/share/send" element={<FileUpload/>}/>
-        <Route path="/share/receive" element={<FileReceive />} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/share">
+          <Route index element={<Navigate to="/share/send" replace />} />
+          <Route path="send" element={<FileUpload />} />
+          <Route path="receive" element={<FileReceive />} />
+        </Route>
       </Routes>
-
-      
     </BrowserRouter>
   );
 }
