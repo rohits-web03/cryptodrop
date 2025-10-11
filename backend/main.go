47ed09df81381e6082356010545c7f5bdce68432
devjit-sarkar <sardevjit7203@gmail.com>
2025-10-11 16:00:56 +0530

    added auth function

--- backend/main.go
+++ backend/main.go
@@ -9,7 +9,7 @@ func main() {
     InitDB()
 
     http.HandleFunc("/register", RegisterHandler)
-
+    http.HandleFunc("/auth", AuthenticateUser)
     fmt.Println("🚀 Server started on :8080")
     http.ListenAndServe(":8080", nil)
 }
