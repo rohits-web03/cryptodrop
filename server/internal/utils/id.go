e4bc841ead872b9f5e308cda777f5be6c61b5a00
rohits-web03 <rohit.dey302003@gmail.com>
2025-10-16 06:21:52 +0530

    update id generation util function to generate secure token for sharing

--- server/internal/utils/id.go
+++ server/internal/utils/id.go
@@ -1,7 +1,15 @@
 package utils
 
-import "github.com/google/uuid"
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
 
-func GenerateID() uuid.UUID {
-	return uuid.New()
+// GenerateSecureToken creates a cryptographically secure random token.
+func GenerateSecureToken(length int) (string, error) {
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(b), nil
 }
