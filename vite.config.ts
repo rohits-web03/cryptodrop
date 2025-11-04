949d593b80a5bbdcce57b839fa57c99d2dc50b6a
rohits-web03 <rohit.dey302003@gmail.com>
2025-11-04 21:29:54 +0530

    Add wrangler for deploying to cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), cloudflare()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
